0494f63c0e52d73d62a5ae719f6c0dd0c90e7b70
chai2010 <chaishushan@gmail.com>
2015-11-18 15:25:15 +0800

    zz

---
+++ internal/tensorflow-cxx/hello.cc
@@ -0,0 +1,10 @@
+// Copyright 2015 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <stdio.h>
+
+int main() {
+	printf("hello tensorflow!\n");
+	return 0;
+}
